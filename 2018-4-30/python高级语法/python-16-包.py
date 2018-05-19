77c33c4050355a5217c0e7d06b1604e46f3672ba
ZoesZhou <38833892+ZoesZhou@users.noreply.github.com>
2018-05-19 21:25:14 +0800

    更新

---
+++ 2018-4-30/python高级语法/python-16-包.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/19 上午 10:24
+# @Author  : Zoe
+# @Site    : 
+# @File    : python-16-包.py
+# @Software: PyCharm
+import p01
+s = p01.Student('Zoe', 19)
+s.say()
+p01.say_hello()
